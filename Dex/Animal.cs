deb0ee227431c009e300a55de863ef414e86fff4
DCP529 <61003286+DCP529@users.noreply.github.com>
2021-07-25 15:29:05 +0300

    Creation of new classes Animal and Dog

---
+++ Dex/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dex
+{
+    abstract class Animal
+
+    {
+        protected double Weight { get; set; }
+        protected double Height { get; set; }
+
+        public Animal(){}
+
+        public Animal(double weight, double height)
+        {
+            Weight = weight;
+            Height = height;
+        }
+
+        public abstract void Voice();
+
+    }
+}
--- Dex/Program.cs
+++ Dex/Program.cs
@@ -6,7 +6,11 @@ namespace Dex
     {
         static void Main(string[] args)
         {
-            
+            Animal dog = new Dog(15, 150);
+            Console.WriteLine("Собака говорит:");
+            dog.Voice();
+
+            Console.ReadLine();
         }
     }
 }
