bb9e645b829e874dc5b80655a5bf64a1521c4109
DCP529 <61003286+DCP529@users.noreply.github.com>
2021-07-31 00:39:42 +0300

    Completed

---
+++ Dex/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dex
+{
+    public class Client
+    {
+        public string Fio { get; set; }
+        public int PassportId { get; set; }
+        public decimal AccountAmount { get; set; }
+
+        public Client(string fio, int passportId, decimal accountAmount)
+        {
+            Fio = fio;
+            PassportId = passportId;
+            AccountAmount = accountAmount;
+        }
+
+    }
+}
--- Dex/Program.cs
+++ Dex/Program.cs
@@ -1,20 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Dex
 {
     class Program
     {
+        public static List<Client> listClient = new List<Client>();
         static void Main(string[] args)
         {
-            Animal dog = new Dog(15, 150);
-            Console.WriteLine("Собака говорит:");
-            dog.Voice();
+            var andrey1 = new Client("Котельцев Андрей Муратович", 1, 15_000);
+            var ivan = new Client("Костюшев Иван Павлович", 2, 1_000);
+            var vladimir = new Client("Рыбников Юрий Антонович", 3, 9_999);
 
-            var cat = new Cat();
-            Console.WriteLine("Кошка говорит:");
-            cat.Voice();
+            listClient.AddRange(new Client[] { ivan, andrey1, vladimir });
+            var client1 = listClient.SearchClient(2);
+            Console.WriteLine(client1.Fio + "\n");
 
-            //cat и dog - наследники Animal, но Voice у них работает по разному
+            var client2 = listClient.SearchMinValue(10_000);
+            foreach (var item in client2)
+            {
+                Console.WriteLine(item.Fio);
+            }
+
+            var client3 = listClient.SearchMin();
+            Console.WriteLine("\n" + client3.Fio);
+
+            var sum = listClient.SumAmount();
+            Console.WriteLine($"\nСумма денег всех клиентов составляет: {sum}");
 
             Console.ReadLine();
         }
